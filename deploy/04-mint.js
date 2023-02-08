486357fa7e429ecec5efa3b6a739f6cde2549d17
Gennadion <ghammurti@gmail.com>
2023-02-08 17:47:50 +0200

    Random IPFS NFT deploy script

--- deploy/04-mint.js
+++ deploy/04-mint.js
@@ -10,4 +10,24 @@ module.exports = async function ({ getNamedAccounts}) {
     await basicMintTx.wait(1)
     console.log(`Basic NFT index 0 has tokenURI: ${await basicNft.tokenURI(0)}`)
 
+    
+    // Random IPFS NFT
+    const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
+    const mintFee = await randomIpfsNft.getMintFee()
+
+    await new Promise(async (resolve, reject) => {
+        setTimeout(resolve, 300000) // 5 mins
+        randomIpfsNft.once("NftMinted", async function () {
+            resolve()
+        })
+        const randomIpfsNftMintTx = await randomIpfsNft.requestNft({value: mintFee.toString()})
+        const randomIpfsNftMintTxReceipt = await randomIpfsNftMintTx.wait(1)
+        if(developmentChains.includes(network.name)){
+            const requestId = randomIpfsNftMintTxReceipt.events[1].args.requestId.toString()
+            const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+            await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address)
+        }
+    })
+
+    console.log(`Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`)
 };
